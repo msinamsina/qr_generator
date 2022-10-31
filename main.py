4165ce6908df404694004b9bf7e2dd7a80efa01a
msinamsina <msinamsina@gmail.com>
2022-10-31 07:03:48 -0400

    update

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ import cv2
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+import arabic_reshaper
+from bidi.algorithm import get_display
+
 
 
 args = argparse.ArgumentParser()
-args.add_argument('--input', type=str, default='input.csv')
+args.add_argument('--input', type=str, default='input.xlsx')
 args.add_argument('--template', type=str, default='badge_template.png')
 args.add_argument('--output_dir', type=str, default='output')
 args.add_argument('--output_file', type=str, default='output.csv')
@@ -23,11 +26,11 @@ if not os.path.exists(args.output_dir):
     os.mkdir(args.output_dir)
 
 out_df = pd.DataFrame(columns=['name', 'surname', 'code'])
-df = pd.read_csv(args.input)
+df = pd.read_excel(args.input)
 
 
-def read_badge_template():
-    img = Image.open(args.template)
+def read_badge_template(path):
+    img = Image.open(path)
     # img = img.resize((img.size[0]*2, img.size[1]*2))
     return img
 
@@ -44,9 +47,9 @@ def generate_qr_code(name, surname):
     hash.update(name.encode('utf-8'))
     hash.update(surname.encode('utf-8'))
     hash.update(str(time.time()).encode('utf-8'))
-    hash = hash.hexdigest()
-    img = qrcode.make(hash[:16])
-    img = img.resize((120, 120))
+    hash = name+surname+hash.hexdigest()[:5]
+    img = qrcode.make(hash)
+    img = img.resize((140, 140))
     return img, hash
 
 
@@ -58,44 +61,23 @@ def add_qr_code(img, qr_code, x, y):
 
 
 for i, row in df.iterrows():
-    flg = 0
-    if i % 4 == 0:
-        badge_img = read_badge_template()
+    print(row['Badge Type'])
+    badge_img = read_badge_template(row['Badge Type']+'.PNG')
 
     w, h = badge_img.size
+    ref_x = w//2
+    ref_y = h//2
 
-    if i % 4 == 0:
-        ref_x = w//4
-        ref_y = h//4
-    elif i % 4 == 1:
-        ref_x = w//4
-        ref_y = h//4*3
-    elif i % 4 == 2:
-        ref_x = w//4*3
-        ref_y = h//4
-    elif i % 4 == 3:
-        ref_x = w//4*3
-        ref_y = h//4*3
-
-    badge_img = draw_text(badge_img, row['name'], ref_x, ref_y -80, ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf', 28), (0, 0, 0))
-    badge_img = draw_text(badge_img, row['pos'], ref_x, ref_y -50, ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf', 24), (0, 0, 0))
-    badge_img = draw_text(badge_img, row['affiliation'], ref_x, ref_y -10, ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf', 20), (0, 0, 0))
-    qrcode_img, hash = generate_qr_code(row['name'], row['affiliation'])
+    badge_img = draw_text(badge_img, get_display(arabic_reshaper.reshape(str(row['Name']))), ref_x, ref_y -80, ImageFont.truetype('times.ttf', 20), (0, 0, 0))
+    badge_img = draw_text(badge_img, get_display(arabic_reshaper.reshape(str(row['Position']))), ref_x, ref_y -50, ImageFont.truetype('times.ttf', 18), (0, 0, 0))
+    badge_img = draw_text(badge_img, get_display(arabic_reshaper.reshape(str(row['Affiliation']))), ref_x, ref_y -10, ImageFont.truetype('times.ttf', 17), (0, 0, 0))
+    qrcode_img, hash = generate_qr_code(str(row['Name']), str(row['Affiliation']))
     final_img = add_qr_code(badge_img, qrcode_img, ref_x, ref_y+80)
 
-    out_df = out_df.append({'name': row['name'], 'surname': row['affiliation'], 'code': hash}, ignore_index=True)
+    out_df = out_df.append({'name': row['Name'], 'surname': row['Affiliation'], 'code': hash}, ignore_index=True)
+    # final_img.show()
+    final_img.save(os.path.join(args.output_dir, hash + '.png'))
 
-    #
-    #
-
-    if i % 4 == 3:
-        final_img.show()
-        flg = 1
-    # # img.save(os.path.join(args.output_dir, hash + '.png'))
-if flg == 0:
-    final_img.show()
-
-
-# out_df.to_csv(args.output_file, index=False)
+out_df.to_csv(args.output_file, index=False)
 
 
